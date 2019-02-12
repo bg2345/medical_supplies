dacf74179218722bee8733addd3295852360d5dc
bg2345 <bgardynski@gmail.com>
2019-02-12 12:22:10 -0500

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+# import Flask class from flask module
+from flask import Flask
+from flask_bootstrap import Bootstrap
+
+
+# create instance of app variable
+app = Flask(__name__)
+
+# all other variable instanes need to come after the app instance
+bootstrap = Bootstrap(app)
+
+# once app variable is created, import the routes to load home page
+from app import routes
