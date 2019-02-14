f73063493e6a3d305b91d162218674c1f54b36c7
bg2345 <bgardynski@gmail.com>
2019-02-14 13:35:05 -0500

    Posts page and database working

--- app/models.py
+++ app/models.py
@@ -4,6 +4,6 @@ from datetime import datetime
 
 
 class Post(db.Model):
-    post_id = db.Column(db.integer, primary_key=True)
-    tweet = db.Column(db.string(140))
+    post_id = db.Column(db.Integer, primary_key=True)
+    tweet = db.Column(db.String(140))
     date_posted = db.Column(db.DateTime, default=datetime.now().date())
