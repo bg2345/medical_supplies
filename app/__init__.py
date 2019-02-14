9decc407ad61b65c7f4dc3f0f2279ed7bd24862d
bg2345 <bgardynski@gmail.com>
2019-02-14 12:09:47 -0500

    forms completed, TODO: add database and models

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 # import Flask class from flask module
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from config import Config
+
 
 
 # create instance of app variable
@@ -8,6 +10,7 @@ app = Flask(__name__)
 
 # all other variable instanes need to come after the app instance
 bootstrap = Bootstrap(app)
+app.config.from_object(Config)
 
 # once app variable is created, import the routes to load home page
 from app import routes
